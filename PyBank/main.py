fb5ea7c0d8206e920146d71211fb0aa20c3b8f9b
oswaldvinueza <Oswald.vinueza@gmail.com>
2019-06-29 22:20:23 -0500

    Final Python Challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,62 @@
-#PyBank
+import csv
+import os
+
+#Load file
+file_to_load = os.path.join("Resources","budget_data.csv")
+
+#Setting variables
+total_months = 0
+total_pnl = 0
+
+pnl_change = 0
+last_pnl = 0
+changes_list =[]
+
+greatest_increase = ["", 0]
+greatest_decrease = ["", 10000000]
+
+#Read the csv 
+with open(file_to_load) as financial_data:
+    reader = csv.DictReader(financial_data)   
+
+    for row in reader:
+        #calculate total months
+        total_months = total_months + 1
+
+        #calculate total profit and loss
+        total_pnl = total_pnl + int(row["Profit/Losses"])
+
+        #calculate greatest increase and decrease by comparing to each value in the changes_list
+        pnl_change  = int(row["Profit/Losses"]) - last_pnl
+
+        last_pnl = int(row["Profit/Losses"])
+
+        if (pnl_change > greatest_increase[1]):
+            greatest_increase[0] = row ["Date"]
+            greatest_increase[1] = pnl_change
+
+        if (pnl_change < greatest_decrease[1]):
+             greatest_decrease[0] = row ["Date"]
+             greatest_decrease[1] = pnl_change 
+
+        changes_list = changes_list + [pnl_change]
+           
+#calculate net average based on all data compuled in the changes list, excluding the first row
+net_average = (sum(changes_list)-changes_list[0])/(len(changes_list)-1)
+
+#Financial Analysis text file
+financial_analysis = (
+    f"Financial Analysis\n"
+    f"------------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${total_pnl}\n"
+    f"Average Change: ${net_average:0.2f}\n"
+    f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+
+print(financial_analysis)
+
+#Output
+file_to_output = "financial_analysis.txt"
+with open(file_to_output, "w") as txt_file:
+    txt_file.write(financial_analysis)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,72 @@
-#PyPoll
+import csv
+import os
+
+#Load file
+file_to_load = os.path.join("Resources","election_data.csv")
+
+#Set variables
+total_votes = 0
+unique_candidate_list = []
+candidate_votes = {}
+election_results =""
+candidate_list=[]
+#Create winner variable
+winner=["",0]
+
+#Start csv reader and convert to list of dictionaries
+with open(file_to_load) as election_data:
+    reader = csv.DictReader(election_data)
+
+    for row in reader:
+        #total number of votes  
+        total_votes = total_votes + 1
+
+        #specify where in csv to get candidate name
+        candidate_name = row["Candidate"]
+
+        #only append candidate name if it is a unique value not already on the list
+        if candidate_name not in unique_candidate_list:
+            unique_candidate_list.append(candidate_name)
+            #start counting how many votes this candidate has
+            candidate_votes[candidate_name] = 0
+
+        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+
+        #print(candidate_votes)
+
+    #list of chosen candidates, percentage of votes for each candidate who were chosen and votes they won
+    #Loop through candidates
+    for candidate in candidate_votes:
+        #Create temp dictionary
+        candidate_info={}
+        votes = candidate_votes[candidate]
+        #Fill dictionary with candidate information
+        candidate_info["votes"] = votes
+        candidate_info["name"] = candidate
+        candidate_info["percentage"] = '{0:.3f}%'.format((votes/total_votes*100))
+        candidate_list.append(candidate_info.copy())
+        #Find winner
+        if(votes>winner[1]):
+            winner[0]=candidate
+            winner[1]=votes
+
+#Election Results Text File
+file_to_output = "election_results.txt"
+with open(file_to_output, "w") as txt_file:
+
+    #Save results to election_results
+    election_results = (
+        f"Election Results\n"
+        f"---------------------------------\n"
+        f"Total Votes: {total_votes}\n"
+        f"---------------------------------\n")
+    
+    for candidate in candidate_list:
+        election_results += f"{candidate['name']}: {candidate['percentage']} ({candidate['votes']})\n"
+        
+    election_results+= (  
+        f"--------------------------------\n"
+        f"Winner: {winner[0]}\n")
+
+    print(election_results)
+    txt_file.write(election_results)
